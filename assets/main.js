7bad21fb8f4b3ab834a9d1f628eb601aeffad873
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-03-29 15:39:23 +0000

    feat: soem JavaScript functionality to contact form

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm){
+    emailjs.send("gmail", "aikido",{
+                "from_name": contactForm.name.value,
+                "from_tel": contactForm.tel.value,
+                "from_email": contactForm.emailaddress.value,
+                "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
