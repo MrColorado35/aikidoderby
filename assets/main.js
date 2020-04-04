2bad1d04784bffb911271c718672e5eb8d0c4b12
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-04-04 15:16:25 +0000

    feat: Contact form works, website is finished as for now. More job to be done after my 2nd Milestone project

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,16 @@ function sendMail(contactForm){
     emailjs.send("gmail", "aikido",{
                 "from_name": contactForm.name.value,
                 "from_tel": contactForm.tel.value,
-                "from_email": contactForm.emailaddress.value,
-                "project_request": contactForm.projectsummary.value
+                "from_email": contactForm.email.value,
+                "message": contactForm.contact.value
     })
     .then(
         function(response) {
-            console.log("SUCCESS", response);
+            alert("SUCCESS! We've got your message and wil answer soon", response);
         },
         function(error) {
-            console.log("FAILED", error);
+            alert("Sending FAILED, please contact Jason via his mobile number underneath.", error);
         }
     );
-    return false;  // To block from loading a new page
+    return false; 
 }
